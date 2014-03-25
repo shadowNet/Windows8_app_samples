753e8ca1b3d6fa9aeeb5cb29fa3d7d506b5ed71e
Ramsey Khadder <ramseykhadder@hotmail.com>
2014-03-25 04:22:34 -0700

    RK - Added sharing

--- quotesTemplate/quotesTemplate/MainPage.xaml.cs
+++ quotesTemplate/quotesTemplate/MainPage.xaml.cs
@@ -4,8 +4,14 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.DataTransfer;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Graphics.Imaging;
+using Windows.Storage;
+using Windows.Storage.Streams;
 using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -38,6 +44,7 @@ namespace quotesTemplate
 
             quotes = new QuotesDataSource().Quotes;
             BindCurrentQoute();
+            RegisterForShare();
         }
 
         private void BindCurrentQoute()
@@ -47,6 +54,41 @@ namespace quotesTemplate
             BackgroundImage.ImageSource = new BitmapImage(quote.BackgroundUri);
             ForegroundImage.ImageSource = new BitmapImage(quote.ForegroundUri);
             QuotesTextBlock.Text = quote.Quote;
+        }
+
+        private void RegisterForShare()
+        {
+            DataTransferManager dataTransferManager = DataTransferManager.GetForCurrentView();
+            dataTransferManager.DataRequested += new TypedEventHandler<DataTransferManager,
+                DataRequestedEventArgs>(this.ShareImageHandler);
+        }
+        
+        private async void ShareImageHandler(DataTransferManager sender,
+            DataRequestedEventArgs e)
+        {
+            DataRequest request = e.Request;
+            request.Data.Properties.Description = "Quote from QuotesTemplate";
+            request.Data.Properties.Title = quotes.CurrentQuote.Quote + "\n\n~" + quotes.CurrentQuote.Name + "\n";
+
+            // Because we are making async calls in the DataRequested event handler,
+            //  we need to get the deferral first.
+            DataRequestDeferral deferral = request.GetDeferral();
+
+            // Make sure we always call Complete on the deferral.
+            try
+            {
+                StorageFile thumbnailFile =
+                    await Package.Current.InstalledLocation.GetFileAsync("DataModel\\Images\\" + quotes.CurrentQuote.BackgroundPath);
+                request.Data.Properties.Thumbnail =
+                    RandomAccessStreamReference.CreateFromFile(thumbnailFile);
+                StorageFile imageFile =
+                    await Package.Current.InstalledLocation.GetFileAsync("DataModel\\Images\\" + quotes.CurrentQuote.ForegroundPath);
+                request.Data.SetBitmap(RandomAccessStreamReference.CreateFromFile(imageFile));
+            }
+            finally
+            {
+                deferral.Complete();
+            }
         }
 
         private void PrevButton_Click(object sender, RoutedEventArgs e)
@@ -75,6 +117,11 @@ namespace quotesTemplate
                 PrevButton_Click(this, new RoutedEventArgs());
             else if (e.Velocities.Linear.X < 0)
                 NextButton_Click(this, new RoutedEventArgs());
+        }
+
+        private void ShareButton_Click(object sender, RoutedEventArgs e)
+        {
+            DataTransferManager.ShowShareUI();
         }
     }
 }
